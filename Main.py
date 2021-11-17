2f1fb1f3e31c58c744f60a366ea52c5db495cbf4
MacRude <magnus.1131@hotmail.com>
2021-11-17 14:31:29 +0100

    Update server

--- Main.py
+++ Main.py
@@ -1,10 +1,8 @@
 import server
 import ObjectTrainer
 import emojis
-
 def greet(who='World'):
     print(f'Hello {who}!')
-
 
 if __name__ == '__main__':
     greet('World')
--- server.py
+++ server.py
@@ -1,9 +1,15 @@
 import math
 from pythonosc import dispatcher
-from pythonosc import server
-import socket
-import threading
+from pythonosc import osc_server
+
 
 if __name__ == "__main__":
     dispatcher = dispatcher.Dispatcher()
-    
+    dispatcher.map("/annotations/thumb", print)  # set address from
+
+    server = osc_server.ThreadingOSCUDPServer(
+        ('localhost', 8081), dispatcher)
+    print("Serving on {}".format(server.server_address))
+    server.serve_forever()
+
+server.close()
