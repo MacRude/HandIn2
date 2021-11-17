8cc4d3795df70509e1320650b680a672e1f579cc
MacRude <magnus.1131@hotmail.com>
2021-11-17 23:22:32 +0100

    Updating to Socket Server

--- server.py
+++ server.py
@@ -1,15 +1,33 @@
-import math
-from pythonosc import dispatcher
-from pythonosc import osc_server
+'''
+	Simple socket server using threads
+'''
 
+import socket
+import sys
 
-if __name__ == "__main__":
-    dispatcher = dispatcher.Dispatcher()
-    dispatcher.map("/annotations/thumb", print)  # set address from
+HOST = ''	# Symbolic name, meaning all available interfaces
+PORT = 8888	# Arbitrary non-privileged port
 
-    server = osc_server.ThreadingOSCUDPServer(
-        ('localhost', 8081), dispatcher)
-    print("Serving on {}".format(server.server_address))
-    server.serve_forever()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print('Socket created')
 
-server.close()
+#Bind socket to local host and port
+try:
+	s.bind((HOST, PORT))
+except socket.error as msg:
+	print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+	sys.exit()
+	
+print('Socket bind complete')
+
+#Start listening on socket
+s.listen(10)
+print('Socket now listening')
+
+#now keep talking with the client
+while 1:
+    #wait to accept a connection - blocking call
+	conn, addr = s.accept()
+	print('Connected with ' + addr[0] + ':' + str(addr[1]))
+	
+s.close()
