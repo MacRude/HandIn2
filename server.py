c429fd5207a3f0eacec0353eaba7619e7852dd4d
MacRude <magnus.1131@hotmail.com>
2021-11-19 15:49:18 +0100

    server works yeet

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 import socket
 import sys
+import select
 
 HOST = ''	# Symbolic name, meaning all available interfaces
 PORT = 8888	# Arbitrary non-privileged port
 #If want to connect to server type: telnet localhost 8888 
 # in new client
+buffer_size = 1024
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print('Socket created')
@@ -22,10 +24,16 @@ print('Socket bind complete')
 s.listen(10)
 print('Socket now listening')
 
+close_socket_condition = 0
 #now keep talking with the client
 while 1:
     #wait to accept a connection - blocking call
 	conn, addr = s.accept()
 	print('Connected with ' + addr[0] + ':' + str(addr[1]))
-	
-s.close()
+	MESSAGE = b'Sup'
+
+	conn.sendto(MESSAGE, (HOST, PORT))
+	break
+
+
+#s.close()
